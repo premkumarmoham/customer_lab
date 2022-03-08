6078f0cd8554e343204423fc4938a259939d975e
premkumarmoham <43033288+premkumarmoham@users.noreply.github.com>
2022-03-08 06:51:38 +0530

    Ui changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 function Footer() {
@@ -12,15 +11,3 @@ function Footer() {
   );
 }
 export default Footer;
-=======
-import React from "react";
-
-function Footer() {
-  return (
-    <footer class="footer font-small blue bg-info">
-      <div class="footer-copyright text-center py-3">Thank You &#128578;</div>
-    </footer>
-  );
-}
-export default Footer;
->>>>>>> 5ba64f5db11065f133067ed9ce5e04a009cdbf92
